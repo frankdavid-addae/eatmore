37aa09fc77aa87174d0583de9e6521e0632f7dfd
Frank David Addae <frankdavid.addae@gmail.com>
2021-10-26 12:45:05 +0000

    welcome screen

--- app/src/main/java/com/example/eatmore/MainActivity.java
+++ app/src/main/java/com/example/eatmore/MainActivity.java
@@ -1,15 +1,28 @@
 package com.example.eatmore;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ConstraintLayout clWelcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTheme(R.style.Theme_Eatmore);
         setContentView(R.layout.activity_main);
+
+        clWelcome = findViewById(R.id.clWelcome);
+
+        clWelcome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clWelcome.setVisibility(View.GONE);
+            }
+        });
     }
 }
