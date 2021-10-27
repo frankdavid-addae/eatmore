29e1dd3e087211c371f0ee79ebf7ae2e6446a92f
Frank David Addae <frankdavid.addae@gmail.com>
2021-10-27 15:36:57 +0000

    food category view holder

---
+++ app/src/main/java/com/example/eatmore/FoodCategoryAdapter.java
@@ -0,0 +1,83 @@
+package com.example.eatmore;
+
+import android.content.Context;
+import android.graphics.PorterDuff;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.card.MaterialCardView;
+
+import java.util.List;
+
+public class FoodCategoryAdapter extends RecyclerView.Adapter<FoodCategoryAdapter.CategoryHolder> {
+
+    List<FoodCategory> foodCategoryList;
+    Context context;
+    int selectedItemPosition = 0;
+
+    public FoodCategoryAdapter(List<FoodCategory> foodCategoryList, Context context) {
+        this.foodCategoryList = foodCategoryList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public CategoryHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        View view = layoutInflater.inflate(R.layout.category_view_holder, parent, false);
+
+        return new CategoryHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoryHolder holder, int position) {
+        holder.tvFoodCategoryName.setText(foodCategoryList.get(position).getName());
+        holder.ivFoodCategoryImage.setImageResource(foodCategoryList.get(position).getImage());
+
+        if (position == selectedItemPosition) {
+            holder.tvFoodCategoryName.setTextColor(context.getColor(R.color.red));
+            holder.ivFoodCategoryImage.setColorFilter(ContextCompat.getColor(context, R.color.red), PorterDuff.Mode.SRC_ATOP);
+            holder.mcvCategory.setOutlineAmbientShadowColor(context.getColor(R.color.red));
+            holder.mcvCategory.setOutlineSpotShadowColor(context.getColor(R.color.red));
+            holder.mcvCategory.setStrokeWidth(2);
+        } else {
+            holder.tvFoodCategoryName.setTextColor(context.getColor(R.color.grey_one));
+            holder.ivFoodCategoryImage.setColorFilter(ContextCompat.getColor(context, R.color.grey_one), PorterDuff.Mode.SRC_ATOP);
+            holder.mcvCategory.setOutlineAmbientShadowColor(context.getColor(R.color.grey_one));
+            holder.mcvCategory.setOutlineSpotShadowColor(context.getColor(R.color.grey_one));
+            holder.mcvCategory.setStrokeWidth(0);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return foodCategoryList.size();
+    }
+
+    class CategoryHolder extends RecyclerView.ViewHolder {
+        TextView tvFoodCategoryName;
+        ImageView ivFoodCategoryImage;
+        MaterialCardView mcvCategory;
+        public CategoryHolder(@NonNull View itemView) {
+            super(itemView);
+            tvFoodCategoryName = itemView.findViewById(R.id.tvFoodCategoryName);
+            ivFoodCategoryImage = itemView.findViewById(R.id.ivFoodCategoryImage);
+            mcvCategory = itemView.findViewById(R.id.mcvCategory);
+
+            mcvCategory.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    selectedItemPosition = getAdapterPosition();
+                    notifyDataSetChanged();
+                }
+            });
+        }
+    }
+}
--- app/src/main/java/com/example/eatmore/HomeActivity.java
+++ app/src/main/java/com/example/eatmore/HomeActivity.java
@@ -1,14 +1,48 @@
 package com.example.eatmore;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeActivity extends AppCompatActivity {
+
+    RecyclerView rvCategories;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        rvCategories = findViewById(R.id.rvCategories);
+
+        setCategories();
+    }
+
+    private void setCategories() {
+        List<FoodCategory> foodCategoryList = new ArrayList<>();
+
+        FoodCategory foodCategory = new FoodCategory("Chicken", R.drawable.ic_chicken);
+        FoodCategory foodCategory2 = new FoodCategory("Burger", R.drawable.ic_burger);
+        FoodCategory foodCategory3 = new FoodCategory("Pizza", R.drawable.ic_pizza);
+        FoodCategory foodCategory4 = new FoodCategory("Chicken", R.drawable.ic_chicken);
+        FoodCategory foodCategory5 = new FoodCategory("Chicken", R.drawable.ic_chicken);
+
+        foodCategoryList.add(foodCategory);
+        foodCategoryList.add(foodCategory2);
+        foodCategoryList.add(foodCategory3);
+        foodCategoryList.add(foodCategory4);
+        foodCategoryList.add(foodCategory5);
+
+        FoodCategoryAdapter foodCategoryAdapter = new FoodCategoryAdapter(foodCategoryList, HomeActivity.this);
+
+        rvCategories.setLayoutManager(new LinearLayoutManager(HomeActivity.this, RecyclerView.HORIZONTAL, false));
+        rvCategories.setAdapter(foodCategoryAdapter);
+
+        foodCategoryAdapter.notifyDataSetChanged();
     }
 }
